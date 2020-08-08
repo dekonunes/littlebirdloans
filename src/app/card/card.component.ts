fc2d53b5cf70ea007d1f07f62f97ccad61e9dfdc
dekonunes <dekonunesss@gmail.com>
2020-08-08 07:36:03 -0300

    cards

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
+  animations: [
+    trigger('cardAnimation', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), { optional: true }),
+        query(':enter', stagger('300ms', [
+          animate('.5s ease-in', keyframes([
+            style({ opacity: 0, transform: 'translateX(-50%)', offset: 0 }),
+            style({ opacity: .5, transform: 'translateX(-10px) scale(1.1)', offset: 0.3 }),
+            style({ opacity: 1, transform: 'translateX(0)', offset: 1 }),
+          ]))]), { optional: true })
+      ]),
+    ])
+  ]
+})
+export class CardComponent {
+  cards = ['Personal Loan', 'Business Loan', 'Debt Consolidation', 'Home Loan', 'Car Loan', 'Student Loan', 'Wedding Loan', 'Moving Home'];
+}
